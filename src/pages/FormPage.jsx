f95510215759b056cb6078535b586db887599f79
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2024-05-23 02:25:15 +0530

    min-height added

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -10,7 +10,7 @@ const FormPage = () => {
     return (
         <>
             <ThreeLineButton onThreeLineClick={() => setShowSidebar(true)} />
-            <div className="w-full relative bg-gradient-to-r from-prymary-blue via-via-color to-prymary-blue overflow-hidden flex flex-col items-start justify-start pt-[20px] px-0 pb-[136px] box-border gap-[29px] leading-[normal] tracking-[normal] text-left text-[25.6px] text-text1 font-body-2 mq1350:h-auto mq1825:pt-16"> {/*check tailwind.config.css for colors combination*/}
+            <div className="min-h-dvh w-full relative bg-gradient-to-r from-prymary-blue via-via-color to-prymary-blue overflow-hidden flex flex-col items-start justify-start pt-[20px] px-0 pb-[136px] box-border gap-[29px] leading-[normal] tracking-[normal] text-left text-[25.6px] text-text1 font-body-2 mq1350:h-auto mq1825:pt-16"> {/*check tailwind.config.css for colors combination*/}
                 <Navbar />
                 <BasicInfo />
                 <div className="fixed !m-[0] top-[0px] bottom-[0px] left-[0px] z-50 min1900:bg-gradient-to-r from-via-color">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const EventsPageorganiser = () => {
   return (
     <>
       <ThreeLineButton onThreeLineClick={() => setShowSidebar(true)} />
-      <div className="w-full bg-gradient-to-r from-prymary-blue via-via-color to-prymary-blue relative overflow-hidden flex flex-col items-start justify-start pt-[20px] px-0 pb-[136px] box-border gap-[29px] leading-[normal] tracking-[normal] text-left text-[25.6px] text-text1 font-body-2 mq1350:h-auto mq1825:pt-16"> {/*check tailwind.config.css for colors combination*/}
+      <div className="min-h-dvh w-full bg-gradient-to-r from-prymary-blue via-via-color to-prymary-blue relative overflow-hidden flex flex-col items-start justify-start pt-[20px] px-0 pb-[136px] box-border gap-[29px] leading-[normal] tracking-[normal] text-left text-[25.6px] text-text1 font-body-2 mq1350:h-auto mq1825:pt-16"> {/*check tailwind.config.css for colors combination*/}
         <Navbar />
         <CreateEvents />
         <EventsContainer />
